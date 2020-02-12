50ff1b543b34d7524e9bb2d8ecb78143f81bce40
Roman Filippov <rf@romanfilippov.com>
2020-02-12 18:39:20 +0700

    fix: fixes type exporting issue

--- src/jest-polly.ts
+++ src/jest-polly.ts
@@ -7,8 +7,16 @@ import { jestPollyConfigService } from './config'
 Polly.register(NodeHttpAdapter)
 Polly.register(FSPersister)
 
+// We have to define our own "Context" here, and can't re-use types from
+// "@types/setup-polly-jest" package, because, for some reason, there is a problem with
+// exporting the type and when importing @scaleleap/jest-polly in consuming modules,
+// the result is typed as "any"
+export interface JestPollyContext {
+  readonly polly: Polly
+}
+
 // first we instantiate a Polly instance with default configuration
-export const jestPollyContext = setupPolly()
+export const jestPollyContext: JestPollyContext = setupPolly()
 
 // and then before each test run, we'll update it with actual configuration, because
 // some of the configuration, depends on being inside a test
