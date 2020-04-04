adbcd144d94b1e020cb8f376b0d352d8bab33e1d
Roman Filippov <rf@romanfilippov.com>
2020-04-04 11:58:39 +0700

    refactor: test server to use promises

--- src/jest-polly.test.ts
+++ src/jest-polly.test.ts
@@ -14,19 +14,39 @@ import './jest-polly'
 
 const RESPONSE = 'Hello World!'
 
-const server = http.createServer((_req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.write(RESPONSE)
-  res.end()
+type Server = ReturnType<typeof http.createServer>
+
+const createServer = () =>
+  new Promise<Server>((resolve, reject) => {
+    const server = http.createServer((_req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.write(RESPONSE)
+      res.end()
+    })
+
+    server
+      .listen(8080)
+      .once('error', reject)
+      .once('listening', () => resolve(server))
+
+    return server
+  })
+
+const destroyServer = (server: Server) =>
+  new Promise((resolve, reject) => {
+    return server
+      .close()
+      .once('error', reject)
+      .once('close', resolve)
+  })
+
+let server: Server
+
+beforeEach(async () => {
+  server = await createServer()
 })
 
-beforeEach(done => {
-  server.listen(8080).once('listening', done)
-})
-
-afterEach(done => {
-  server.close().once('close', done)
-})
+afterEach(() => destroyServer(server))
 
 test('replays recording', async done => {
   expect.assertions(1)
