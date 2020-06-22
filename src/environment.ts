b4afd68e9af2d2ca3e63315d23aa5d30238d4887
Roman Filippov <rf@romanfilippov.com>
2020-06-22 09:50:14 +0700

    chore: upgrades @scaleleap/config

--- src/environment.ts
+++ src/environment.ts
@@ -5,7 +5,7 @@ import { isCI } from 'ci-info'
 const POLLY_MODES: MODE[] = ['replay', 'record', 'passthrough', 'stopped']
 
 class Config extends BaseConfig {
-  readonly POLLY_MODE = this.get('POLLY_MODE').default(POLLY_MODES[0]).asEnum(POLLY_MODES) as MODE
+  readonly POLLY_MODE = this.get('POLLY_MODE').default(POLLY_MODES[0]).asEnum<MODE>(POLLY_MODES)
 
   readonly POLLY_RECORD_IF_MISSING = this.get('POLLY_RECORD_IF_MISSING')
     .default(isCI ? 'false' : 'true')
