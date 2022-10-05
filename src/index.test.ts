266a9d3eecf18a0c835b6c7e9e75e8419969e7de
Toan Nguyen <nguyentoandev@gmail.com>
2022-10-05 10:14:33 +0700

    ci: update codecov ci node version

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,6 +5,11 @@ import { jestPollyConfigService, jestPollyContext } from '.'
 import { JestPollyConfigService } from './config'
 
 describe('index', () => {
+  /**
+   * Didn't figure out the cause of this weird failing test.
+   *
+   * @see https://github.com/ScaleLeap/jest-polly/pull/303#discussion_r986363886
+   */
   // eslint-disable-next-line jest/no-disabled-tests
   it.skip('exports polly instance', () => {
     expect.assertions(1)
